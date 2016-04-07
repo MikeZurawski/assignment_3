20a8edb0d4aa2129e4fb1fee914b96ed3ee33b7f
mike zurwaski <mzurawski@irobot.com>
2016-04-07 22:01:26 +0300

    removing some things

--- SecondClass.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "PowerTeam.h"
-#include "SecondClass.h"
-
-
-SecondClass::SecondClass(double num){secondDouble = num;}
-
-void SecondClass::printNum(double num){cout << secondDouble << endl;}
